2454ef7f9874e3b544e29f4c6269fd023a191bad
Julio Soto <juliosotodev@gmail.com>
2022-12-05 23:59:05 -0700

    day 1 file read

--- day01/AoC2022_day01.py
+++ day01/AoC2022_day01.py
@@ -1,3 +1,7 @@
 # Advent of code puzzle Day 1
 
-
+calories = []
+with open('input_day01.txt', 'r') as f:
+    for line in f:
+        if line.strip():  # Skip blank lines.
+            calories.append(int(line))    
