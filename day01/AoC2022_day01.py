c512517b9f3c18b684231bf99085bca9077268dc
Julio Soto <juliosotodev@gmail.com>
2022-12-07 23:58:03 -0700

    getting rid of blank lines

--- day01/AoC2022_day01.py
+++ day01/AoC2022_day01.py
@@ -1,7 +1,12 @@
 # Advent of code puzzle Day 1
 
 calories = []
-with open('input_day01.txt', 'r') as f:
-    for line in f:
-        if line.strip():  # Skip blank lines.
-            calories.append(int(line))    
+
+inputData = open('./day01/input_day01.txt','r')
+for line in inputData:
+    if line.strip():
+        calories.append(line)
+
+inputData.close()
+
+print(calories)
