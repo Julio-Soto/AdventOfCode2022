7eab17c36b923b06474ad44c4ffa2662b0285c72
Julio Soto <juliosotodev@gmail.com>
2022-12-08 23:50:54 -0700

    get top 3 sum. solution day1-2

--- day01/AoC2022_day01.py
+++ day01/AoC2022_day01.py
@@ -3,6 +3,9 @@
 groupCalories = []
 calorieSum = 0
 maxCalories = 0
+nunberOfElves = 0
+top3ElvesCalories = 0
+sortedGroupCalories = []
 
 caloriesData = open('./day01/input_day01.txt','r')
 singleCalories = caloriesData.readlines()
@@ -15,9 +18,17 @@ for singleCalorieAmount in singleCalories:
         groupCalories.append(calorieSum)
         calorieSum = 0
 
-
 for groupCalorieAmount in groupCalories:
     maxCalories = max(maxCalories,groupCalorieAmount)
 
-print(groupCalories)
-print(maxCalories)
+print('Calories carried by top elf: ' + str(maxCalories))
+
+# PART 2
+
+sortedGroupCalories = sorted(groupCalories)
+
+numberOfElves = len(sortedGroupCalories)
+for i in range(1,4):
+    top3ElvesCalories += sortedGroupCalories[numberOfElves - i]
+
+print('Calorie sum of top 3 elves: ' + str(top3ElvesCalories))
