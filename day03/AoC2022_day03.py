cbdf05d518a86b06bec40ecaf3f148eb678c8b9f
Julio Soto <juliosotodev@gmail.com>
2022-12-09 23:56:25 -0700

    Day 3 read file and data setup

--- day03/AoC2022_day03.py
+++ day03/AoC2022_day03.py
@@ -1,3 +1,11 @@
 # Advent Of Code 2022 Day 3
 # PART 1
 
+prioritySum = 0
+leftSide = ''
+rightSide = ''
+
+rucksacks = open('./day03/input_day03.txt','r')
+
+print('Number of Rucksacks: ', len(rucksacks.readlines()))
+
