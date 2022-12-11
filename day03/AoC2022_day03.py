30a161b584e39c6f8752114ccea7fec40b27497c
Julio Soto <juliosotodev@gmail.com>
2022-12-10 23:39:34 -0700

    Day 3 changed the read file method

--- day03/AoC2022_day03.py
+++ day03/AoC2022_day03.py
@@ -5,7 +5,10 @@ prioritySum = 0
 leftSide = ''
 rightSide = ''
 
-rucksacks = open('./day03/input_day03.txt','r')
+rucksacksData = open('./day03/input_day03.txt','r')
+rucksacksList = rucksacksData.read().splitlines()
 
-print('Number of Rucksacks: ', len(rucksacks.readlines()))
+print('Number of Rucksacks: ', len(rucksacksList))
 
+print(len(rucksacksList[0]))
+print(rucksacksList[:3])
