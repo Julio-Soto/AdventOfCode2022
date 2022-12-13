a118f624ffcb9e609506d609a02f7e36fe6bd290
Julio Soto <juliosotodev@gmail.com>
2022-12-12 23:54:18 -0700

    Day 3 Part 2. rucksack comparision loop

--- day03/AoC2022_day03.py
+++ day03/AoC2022_day03.py
@@ -2,12 +2,16 @@
 # PART 1
 
 prioritySum = 0
+groupPrioritySum = 0
+currentRuckSack = 0
+foundMatch = False
+matchedItem = ''
 leftSide =  ''
 rightSide = ''
 
 rucksacksData = open('./day03/input_day03.txt','r')
 rucksacksList = rucksacksData.read().splitlines()
-
+"""
 print('Number of Rucksacks: ', len(rucksacksList))
 
 for idx, ruckSack in enumerate(rucksacksList):
@@ -21,3 +25,30 @@ for idx, ruckSack in enumerate(rucksacksList):
             break
 
 print('Sum of Priorities: ', prioritySum)
+"""
+print(range(1,len(rucksacksList)))
+
+#while len(rucksacksList) != 0:
+for idx in range(1,len(rucksacksList)):
+    print('matching rucksack index : ', idx)
+    for item in rucksacksList[currentRuckSack]:
+        print(currentRuckSack, ' item: ',item)
+        if rucksacksList[idx].find(item) >=0 :
+            rucksacksList.pop(currentRuckSack)
+            currentRuckSack = idx
+            matchedItem = item
+            foundMatch = True
+            print('found match in rucksack: ', idx)
+            break
+    if foundMatch == True:
+        print('BREAK')
+        break
+
+
+'''
+for idx, ruckSack in enumerate(rucksacksList):
+    for item in ruckSack:
+        if rucksacksList[idx + 1].find(item) >= 0:
+            rucksacksList.pop(idx)
+        break
+'''
