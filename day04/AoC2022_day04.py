c370c18bb8358fa4751e63b333a5ef9653673bba
Julio Soto <juliosotodev@gmail.com>
2022-12-13 03:31:00 -0700

    day 4 file open and string elements parsing

--- day04/AoC2022_day04.py
+++ day04/AoC2022_day04.py
@@ -1,3 +1,13 @@
 # Advent Of Code 2022 Puzzle Day 4
 # PART 1
 
+containedTotal = 0
+leftPair = []
+rightPair = []
+
+pairList = (open('./day04/input_day04.txt','r')).read().splitlines()
+
+leftPair = pairList[0].split(',')[0].split('-')
+rightPair = pairList[0].split(',')[1].split('-')
+
+print(rightPair[1])
