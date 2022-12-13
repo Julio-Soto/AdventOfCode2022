b35220c649bb78ae01b57fff239b5d0ce941c631
Julio Soto <juliosotodev@gmail.com>
2022-12-13 04:07:05 -0700

    day 4 PART 1 soln. loop comparing difference signs

--- day04/AoC2022_day04.py
+++ day04/AoC2022_day04.py
@@ -1,13 +1,17 @@
 # Advent Of Code 2022 Puzzle Day 4
 # PART 1
 
-containedTotal = 0
+notContainedTotal = 0
 leftPair = []
 rightPair = []
 
 pairList = (open('./day04/input_day04.txt','r')).read().splitlines()
 
-leftPair = pairList[0].split(',')[0].split('-')
-rightPair = pairList[0].split(',')[1].split('-')
+for pair in pairList:
+    leftPair = pair.split(',')[0].split('-')
+    rightPair = pair.split(',')[1].split('-')
+    print(leftPair, ' ', rightPair)
+    if int(leftPair[0]) - int(rightPair[0]) > 0 and int(leftPair[1]) - int(rightPair[1]) > 0: notContainedTotal += 1
+    if int(leftPair[0]) - int(rightPair[0]) < 0 and int(leftPair[1]) - int(rightPair[1]) < 0: notContainedTotal += 1
 
-print(rightPair[1])
+print('Contained Total: ', (len(pairList) - notContainedTotal)) 
