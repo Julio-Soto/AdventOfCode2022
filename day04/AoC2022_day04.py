ce423bffdcf134b9ce02d1e76b35f2c8a0d5d29e
Julio Soto <juliosotodev@gmail.com>
2022-12-13 18:05:19 -0700

    day 4 PART 2 soln. loop comparing pair edges

--- day04/AoC2022_day04.py
+++ day04/AoC2022_day04.py
@@ -2,6 +2,7 @@
 # PART 1
 
 notContainedTotal = 0
+notOverlap = 0
 leftPair = []
 rightPair = []
 
@@ -10,8 +11,18 @@ pairList = (open('./day04/input_day04.txt','r')).read().splitlines()
 for pair in pairList:
     leftPair = pair.split(',')[0].split('-')
     rightPair = pair.split(',')[1].split('-')
-    print(leftPair, ' ', rightPair)
+    #print(leftPair, ' ', rightPair)
     if int(leftPair[0]) - int(rightPair[0]) > 0 and int(leftPair[1]) - int(rightPair[1]) > 0: notContainedTotal += 1
     if int(leftPair[0]) - int(rightPair[0]) < 0 and int(leftPair[1]) - int(rightPair[1]) < 0: notContainedTotal += 1
 
 print('Contained Total: ', (len(pairList) - notContainedTotal)) 
+
+# PART 2
+
+for pair in pairList:
+    leftPair = pair.split(',')[0].split('-')
+    rightPair = pair.split(',')[1].split('-')
+    if int(leftPair[1]) < int(rightPair[0]): notOverlap += 1
+    if int(leftPair[0]) > int(rightPair[1]): notOverlap += 1
+
+print('Overlap Anywhere: ',len(pairList) - notOverlap)
