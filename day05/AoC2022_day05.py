fc2d811e5bf1265a5b7d2a68034e7cc13a0e5081
Julio Soto <juliosotodev@gmail.com>
2022-12-14 23:56:34 -0700

    day 5 PART 1. read stack data

--- day05/AoC2022_day05.py
+++ day05/AoC2022_day05.py
@@ -1,2 +1,9 @@
 # Advent Of Code 2022 Puzzle Day 5
 # PART 1
+
+numOfStackInputLines = 8
+
+inputLines = open('./day05/input_day05.txt','r')
+
+for i in range(numOfStackInputLines):
+    print(inputLines.readline())
