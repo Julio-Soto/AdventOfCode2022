8cf35fdeb347a97edc67632b83c1746407ca8a6a
Julio Soto <juliosotodev@gmail.com>
2022-12-15 03:22:13 -0700

    day 5 PART 1. stacks data model

--- day05/AoC2022_day05.py
+++ day05/AoC2022_day05.py
@@ -2,8 +2,22 @@
 # PART 1
 
 numOfStackInputLines = 8
+stacks = [[]]
+stacksLines = []
+movesLines = []
 
-inputLines = open('./day05/input_day05.txt','r')
+inputLines = open('./day05/input_day05.txt','r').read().splitlines()
 
+# READ THE STACK INPUT LINES.
 for i in range(numOfStackInputLines):
-    print(inputLines.readline())
+    stacksLines.append(inputLines[i])
+    print(stacksLines[i])
+
+# STACKS LIST WILL BE THE MODEL OF THE STACKS.
+for stackIdx in range(1,10):
+    stacks.append(list())
+    for itemIdx in range(numOfStackInputLines-1,-1,-1):
+        if stacksLines[itemIdx][(4 * stackIdx) - 3] != ' ':
+            stacks[stackIdx].append(stacksLines[itemIdx][(4 * stackIdx) - 3])
+
+print(stacks[4][5])
