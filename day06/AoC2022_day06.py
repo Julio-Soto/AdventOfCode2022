be80dd67983f6b5c09d5f38edb5a36cd1aa8a77e
Julio Soto <juliosotodev@gmail.com>
2022-12-16 02:18:47 -0700

    day 6 PART 1. Solution. Iterate with substrings

--- day06/AoC2022_day06.py
+++ day06/AoC2022_day06.py
@@ -2,3 +2,17 @@
 # PART 1
 
 # RAW DATA FROM FILE
+
+str = ''
+startChar = 4
+
+dataStreamFile = open('./day06/input_day06.txt')
+dataStream = dataStreamFile.read()
+dataStreamFile.close()
+
+for currentIDX in range(startChar,len(dataStream)):
+    str = dataStream[currentIDX-4 : -len(dataStream) + currentIDX]
+    if len(str) == len(set(str)):
+        print(currentIDX)
+        print(str)
+        break
