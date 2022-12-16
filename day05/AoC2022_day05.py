b1006644731a54d042156216a58697b0ffec247d
Julio Soto <juliosotodev@gmail.com>
2022-12-15 21:15:20 -0700

    day 5 PART 1. move items function

--- day05/AoC2022_day05.py
+++ day05/AoC2022_day05.py
@@ -10,6 +10,7 @@ movesLines = []
 #MODEL DATA FOR PROCESSING
 stacks = [[]]
 moves = []
+movingItem = ''
 
 inputLines = open('./day05/input_day05.txt','r').read().splitlines()
 
@@ -18,12 +19,12 @@ for i in range(numOfStackInputLines):
     stacksLines.append(inputLines[i])
     print(stacksLines[i])
 
-# READ THE MOVES DATA INPUT LINES AND CLEAN IT.
+# POPULATE MOVES LIST. READ THE MOVES DATA INPUT LINES AND CLEAN IT.
 for lineNum in range (numOfStackInputLines + separation, len(inputLines)):
     moves.append((inputLines[lineNum].replace('move ','').replace('from ','').replace('to ','')).split())
 
 
-# STACKS LIST WILL BE THE MODEL OF THE STACKS.
+# POPULATE STACKS LIST. IT WILL BE THE MODEL OF THE STACKS.
 for stackIdx in range(1,10):
     stacks.append(list())
     for itemIdx in range(numOfStackInputLines-1,-1,-1):
@@ -31,13 +32,27 @@ for stackIdx in range(1,10):
             stacks[stackIdx].append(stacksLines[itemIdx][(4 * stackIdx) - 3])
 
 # PRINT THE DATA MODEL CURRENT STATE.
-for i in range(1,10):
-    print(str(i) + ' ', end='')
-    for item in stacks[i]:
-        print(item,end='')
+def stackPrint():
+    for i in range(1,10):
+        print(str(i) + ' ', end='')
+        for item in stacks[i]:
+            print(item,end='')
+        print('')
     print('')
+
 # ACCESS A SINGLE ITEM BY STACK NUMBER AND ITEM INDEX.
 #print(stacks[4][5])
 
+print('Initial State:')
+stackPrint()
 
+def moveItems(number,fromStack,toStack):
+    for k in range(number):
+        movingItem = stacks[fromStack][len(stacks[fromStack])-1]
+        stacks[toStack].append(movingItem)
+        stacks[fromStack].pop()
 
+moveItems(6,5,7)
+
+print('Final State:')
+stackPrint()
