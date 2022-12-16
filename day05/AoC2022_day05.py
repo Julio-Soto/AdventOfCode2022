fe38fffea58e6df4afcb7934d67cecd00f5e5e8a
Julio Soto <juliosotodev@gmail.com>
2022-12-15 20:42:13 -0700

    day 5 PART 1. moves data model

--- day05/AoC2022_day05.py
+++ day05/AoC2022_day05.py
@@ -1,10 +1,15 @@
 # Advent Of Code 2022 Puzzle Day 5
 # PART 1
 
+# RAW DATA FROM FILE
 numOfStackInputLines = 8
-stacks = [[]]
-stacksLines = []
+separation =  2     # number of lines that separate stack and moves data.
+stacksLines =  []
 movesLines = []
+
+#MODEL DATA FOR PROCESSING
+stacks = [[]]
+moves = []
 
 inputLines = open('./day05/input_day05.txt','r').read().splitlines()
 
@@ -13,6 +18,11 @@ for i in range(numOfStackInputLines):
     stacksLines.append(inputLines[i])
     print(stacksLines[i])
 
+# READ THE MOVES DATA INPUT LINES AND CLEAN IT.
+for lineNum in range (numOfStackInputLines + separation, len(inputLines)):
+    moves.append((inputLines[lineNum].replace('move ','').replace('from ','').replace('to ','')).split())
+
+
 # STACKS LIST WILL BE THE MODEL OF THE STACKS.
 for stackIdx in range(1,10):
     stacks.append(list())
@@ -20,4 +30,14 @@ for stackIdx in range(1,10):
         if stacksLines[itemIdx][(4 * stackIdx) - 3] != ' ':
             stacks[stackIdx].append(stacksLines[itemIdx][(4 * stackIdx) - 3])
 
-print(stacks[4][5])
+# PRINT THE DATA MODEL CURRENT STATE.
+for i in range(1,10):
+    print(str(i) + ' ', end='')
+    for item in stacks[i]:
+        print(item,end='')
+    print('')
+# ACCESS A SINGLE ITEM BY STACK NUMBER AND ITEM INDEX.
+#print(stacks[4][5])
+
+
+
