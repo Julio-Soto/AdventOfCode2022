5a77fc676f2ba28b6c12c827f828bf626c3f0c0f
Julio Soto <juliosotodev@gmail.com>
2022-12-18 00:00:03 -0700

    file read

--- day07/AoC2022_day07.py
+++ day07/AoC2022_day07.py
@@ -2,3 +2,12 @@
 # PART 1
 
 # RAW DATA FROM FILE
+
+inputLinesFile = open('./day07/input_day07.txt','r')
+
+inputLines =  inputLinesFile.read().splitlines()
+
+for i, line in enumerate(inputLines):
+    inputLines[i] = line.split()
+    print(inputLines[i])
+    
